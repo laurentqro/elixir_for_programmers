ca53760f0442a3bbd5d08f93adca0223faf33269
Laurent Curau <laurentcurau@gmail.com>
2018-06-27 22:24:21 +0100

    Refactor: tally is public API concern

--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -13,28 +13,28 @@ defmodule HangmanTest do
   test "state isn't changed for :won or :lost game" do
     for state <- [:won, :lost] do
       game = Game.new_game() |> Map.put(:game_state, state)
-      assert { ^game, _ } = Game.make_move(game, "x")
+      assert ^game = Game.make_move(game, "x")
     end
   end
 
   test "first occurence of letter is not already used" do
     game = Game.new_game()
-    { game, _tally } = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state != :already_used
   end
 
   test "second occurence of letter is already used" do
     game = Game.new_game()
-    { game, _tally } = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state != :already_used
 
-    { game, _tally } = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "a good guess is recognized" do
     game = Game.new_game("wibble")
-    { game, _tally } = Game.make_move(game, "w")
+    game = Game.make_move(game, "w")
 
     assert game.game_state == :good_guess
     assert game.turns_left == 7
@@ -42,19 +42,19 @@ defmodule HangmanTest do
 
   test "a guessed word is a won game" do
     game = Game.new_game("wibble")
-    { game, _tally } = Game.make_move(game, "w")
-    { game, _tally } = Game.make_move(game, "i")
-    { game, _tally } = Game.make_move(game, "b")
-    { game, _tally } = Game.make_move(game, "b")
-    { game, _tally } = Game.make_move(game, "l")
-    { game, _tally } = Game.make_move(game, "e")
+    game = Game.make_move(game, "w")
+    game = Game.make_move(game, "i")
+    game = Game.make_move(game, "b")
+    game = Game.make_move(game, "b")
+    game = Game.make_move(game, "l")
+    game = Game.make_move(game, "e")
 
     assert game.game_state == :won
   end
 
   test "a bad guess is recognized" do
     game = Game.new_game("foo")
-    { game, _tally } = Game.make_move(game, "a")
+    game = Game.make_move(game, "a")
 
     assert game.turns_left == 6
     assert game.game_state == :bad_guess
@@ -62,13 +62,13 @@ defmodule HangmanTest do
 
   test "no turns left is a lost game" do
     game = Game.new_game("foo")
-    { game, _tally } = Game.make_move(game, "a")
-    { game, _tally } = Game.make_move(game, "b")
-    { game, _tally } = Game.make_move(game, "c")
-    { game, _tally } = Game.make_move(game, "d")
-    { game, _tally } = Game.make_move(game, "e")
-    { game, _tally } = Game.make_move(game, "g")
-    { game, _tally } = Game.make_move(game, "h")
+    game = Game.make_move(game, "a")
+    game = Game.make_move(game, "b")
+    game = Game.make_move(game, "c")
+    game = Game.make_move(game, "d")
+    game = Game.make_move(game, "e")
+    game = Game.make_move(game, "g")
+    game = Game.make_move(game, "h")
 
     assert game.game_state == :lost
   end
